b2f3075fab7c51f828a703e3bab5f32111c6512d
BrunoH300 <116306479+BrunoH300@users.noreply.github.com>
2023-09-25 20:39:57 -0300

    Update listaExercicios02.sql
    commit 2

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -5,3 +5,14 @@ BEGIN
     SELECT * FROM Autores;
 END;
 }
+Questão 2{
+  CREATE PROCEDURE sp_LivrosPorCategoria
+    Categoria NVARCHAR(50)
+AS
+BEGIN
+    SELECT Livros.Titulo
+    FROM Livros
+    INNER JOIN Categorias ON Livros.CategoriaID = Categorias.ID
+    WHERE Categorias.Nome = Categoria;
+END;
+}
